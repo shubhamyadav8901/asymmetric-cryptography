3149d20975b37d7d743bc10f0e3ec1df64fb2209
Shubham Yadav <luckyshubham16@gmail.com>
2022-09-09 02:30:25 +0530

    fixed favicon error

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-var express = require('express');
-var i18n = require('i18n');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var i18n = require("i18n");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
+var routes = require("./routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(
+  favicon(
+    path.join(
+      __dirname,
+      "public",
+      "cryptography-png-secure-network-icon-clipart.png"
+    )
+  )
+);
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(i18n.init);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -35,30 +43,29 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
 i18n.configure({
-  locales:['en', 'nl'],
-  directory: __dirname + '/locales'
+  locales: ["en", "nl"],
+  directory: __dirname + "/locales",
 });
-
 
 module.exports = app;
